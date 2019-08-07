00f220a8381220021a15ee1856b104c87e898762
opcode <star.absorber@gmail.com>
2019-08-07 14:50:07 +0300

    Реакторинг.

--- tools.go
+++ tools.go
@@ -1,9 +1,9 @@
 package tools
 
 import (
-	"time"
 	"crypto/md5"
 	"encoding/hex"
+	"time"
 )
 
 func PreventExit() {
@@ -16,7 +16,4 @@ func PreventExit() {
 func MD5(text string) string {
 	hash := md5.Sum([]byte(text))
 	return hex.EncodeToString(hash[:])
-	//hasher := md5.New()
-	//hasher.Write([]byte(text))
-	//return hex.EncodeToString(hasher.Sum(nil))
 }
